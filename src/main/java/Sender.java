d65ee853343e4220ec85f6bd277480a7aa4b2f0b
natascha <natalia.andrienko@iais.fraunhofer.de>
2021-05-01 12:55:30 +0200

    Added HUP class: Hist, User, Password (not in GIT)

--- src/main/java/Sender.java
+++ src/main/java/Sender.java
@@ -3,17 +3,14 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 public class Sender {
-  public static String host = "83.212.170.63";
-  public static String username = "tapas";
-  public static String password = "2a55f70a841f18b97c3a7db939b7adc9e34a0f1b";
 
   public void sendMessage(String queue, String message) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost(host);
+    factory.setHost(HUP.host);
     factory.setPort(5672);
     factory.setVirtualHost("tapas-vhost");
-    factory.setUsername(username);
-    factory.setPassword(password);
+    factory.setUsername(HUP.username);
+    factory.setPassword(HUP.password);
 
     try(Connection connection = factory.newConnection()){
       Channel channel = connection.createChannel();
